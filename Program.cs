67c40a7fc3e292f3f7ac2ff8a4f30b2ddab2f1e4
FIC <FIC@info.local>
2017-11-28 19:13:09 -0200

    Criação

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lambda2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*List<string> cidades = new List<string>();
+            cidades.Add("Campinas");
+            cidades.Add("Guarulhos");
+            cidades.Add("Salvador");
+            cidades.Add("Belo Horizonte");
+
+            var cid = cidades.Where(x => x.Length < 10);
+            foreach(string t in cid)
+                Console.WriteLine(t.ToUpper());;*/
+
+            
+
+            
+        }
+    }
+}
