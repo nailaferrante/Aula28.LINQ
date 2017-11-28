5f24b58f5ef05dfad283c3d6c728b02c24e40adb
FIC <FIC@info.local>
2017-11-28 19:33:18 -0200

    Exemplo 2

---
+++ Alunos.cs
@@ -0,0 +1,13 @@
+namespace lambda2
+{
+    public class Alunos
+    {
+        //Exemplo 2
+        /*public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }*/
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace lambda2
     {
         static void Main(string[] args)
         {
-            
+            //Exemplo 1
             /*List<string> cidades = new List<string>();
             cidades.Add("Campinas");
             cidades.Add("Guarulhos");
@@ -19,7 +19,17 @@ namespace lambda2
             foreach(string t in cid)
                 Console.WriteLine(t.ToUpper());;*/
 
+            //Exemplo 2
+            /*List<Alunos> aluno = new List<Alunos>(){
+                new Alunos{Id=20, Nome="Fernanda",Email="fernanda@terra.com.br",Idade=20},
+                new Alunos{Id=15, Nome="Tadeu",Email="tadeu@terra.com.br",Idade=55},
+                new Alunos{Id=56, Nome="Pablo",Email="pablo@terra.com.br",Idade=18},
+                new Alunos{Id=68, Nome="Martha",Email="marthaa@terra.com.br",Idade=28}
+            };
             
+            var al = from x in aluno where x.Idade >20 && x.Idade <50 select x;
+            foreach(Alunos w in al)
+                Console.WriteLine(w.Nome+","+w.Email+","+w.Idade);*/
 
             
         }
